346ba1707c2605624af5166057090368a9e8dcff
zannabianca1997 <zannabianca199712@gmail.com>
2023-09-06 15:07:18 +0200

    removed generic const

--- solutions/2015/4/src/lib.rs
+++ solutions/2015/4/src/lib.rs
@@ -1,22 +1,17 @@
-#![feature(generic_const_exprs)]
-
 pub fn part1(input: &str) -> i64 {
-    mine::<5>(input)
+    mine::<2, true>(input)
 }
 
 pub fn part2(input: &str) -> i64 {
-    mine::<6>(input)
+    mine::<3, false>(input)
 }
 
-fn mine<const N: usize>(input: &str) -> i64
-where
-    [(); N / 2]:,
-{
+fn mine<const N_HALF: usize, const N_ODD: bool>(input: &str) -> i64 {
     for i in 1i64.. {
         let hash: [u8; 16] = md5::compute(format!("{input}{i}").as_bytes()).into();
-        if hash[..N / 2] == [0; N / 2]
-            && if N % 2 != 0 {
-                hash[N / 2] & 0xF0 == 0
+        if hash[..N_HALF] == [0; N_HALF]
+            && if N_ODD {
+                hash[N_HALF] & 0xF0 == 0
             } else {
                 true
             }
