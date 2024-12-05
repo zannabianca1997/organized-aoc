89ac4d44e83694ef5c90dccd5c51f0f8560acaaa
zannabianca1997 <zannabianca199712@gmail.com>
2024-12-05 08:17:15 +0100

    2024 day 5: Optimized ssorted check by using the standard library provided function instead of rolling by own hashmap based

--- solutions/2024/5/src/lib.rs
+++ solutions/2024/5/src/lib.rs
@@ -1,8 +1,6 @@
-use std::{
-    cmp::Ordering,
-    collections::{HashMap, HashSet},
-    hash::{BuildHasherDefault, DefaultHasher, RandomState},
-};
+use std::cmp::Ordering;
+
+use fnv::FnvHashSet;
 
 pub fn parse(
     input: &str,
@@ -24,59 +22,54 @@ pub fn parse(
 
 pub fn part1(input: &str) -> usize {
     let (rules, updates) = parse(input);
-    let rules: Box<[_]> = rules.collect();
+    let rules: FnvHashSet<_> = rules.collect();
+    let order_by_rules = comparing_fn(&rules);
     updates
         .filter_map(|update| {
             let update: Box<[_]> = update.collect();
-            let pos: HashMap<_, _, BuildHasherDefault<DefaultHasher>> =
-                HashMap::from_iter(update.iter().copied().enumerate().map(|(i, v)| (v, i)));
-            for (a, b) in &rules {
-                if let (Some(pa), Some(pb)) = (pos.get(a), pos.get(b)) {
-                    if pa > pb {
-                        return None;
-                    }
-                }
-            }
-            Some(update[update.len() / 2])
+            update
+                .is_sorted_by(order_by_rules)
+                .then(|| update[update.len() / 2])
         })
         .sum()
 }
 
 pub fn part2(input: &str) -> usize {
     let (rules, updates) = parse(input);
-    let rules: HashSet<_, BuildHasherDefault<DefaultHasher>> = rules.collect();
+    let rules: FnvHashSet<_> = rules.collect();
+    let order_by_rules = comparing_fn(&rules);
+    let sort_by_rules = sorting_fn(&rules);
     updates
         .filter_map(|update| {
             let update: Box<[_]> = update.collect();
-            let pos: HashMap<_, _, BuildHasherDefault<DefaultHasher>> =
-                HashMap::from_iter(update.iter().copied().enumerate().map(|(i, v)| (v, i)));
-            let mut ordered = true;
-            for (a, b) in &rules {
-                if let (Some(pa), Some(pb)) = (pos.get(a), pos.get(b)) {
-                    if pa > pb {
-                        ordered = false;
-                    }
-                }
-            }
-            if ordered {
+            if update.is_sorted_by(order_by_rules) {
                 return None;
             }
             // This is an unordered line
             let mut update = update;
-            update.sort_unstable_by(|a, b| {
-                if rules.contains(&(*a, *b)) {
-                    // We know that a should come before b, so it must be less
-                    Ordering::Less
-                } else if rules.contains(&(*b, *a)) {
-                    // We know that a should come after b, so it must be greather
-                    Ordering::Greater
-                } else {
-                    // We don't care how you order those two
-                    Ordering::Equal
-                }
-            });
+            update.sort_unstable_by(sort_by_rules);
 
             Some(update[update.len() / 2])
         })
         .sum()
+}
+
+fn comparing_fn(rules: &FnvHashSet<(usize, usize)>) -> impl Fn(&usize, &usize) -> bool + Copy + '_ {
+    |a, b| !rules.contains(&(*b, *a))
+}
+fn sorting_fn(
+    rules: &FnvHashSet<(usize, usize)>,
+) -> impl Fn(&usize, &usize) -> Ordering + Copy + '_ {
+    |a, b| {
+        if rules.contains(&(*a, *b)) {
+            // We know that a should come before b, so it must be less
+            Ordering::Less
+        } else if rules.contains(&(*b, *a)) {
+            // We know that a should come after b, so it must be greather
+            Ordering::Greater
+        } else {
+            // We don't care how you order those two
+            Ordering::Equal
+        }
+    }
 }
