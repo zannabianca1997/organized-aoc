9d47bda6dad18fc09cf3276cf6f101c7d172b77d
zannabianca1997 <zannabianca199712@gmail.com>
2024-12-07 07:57:48 +0100

    2024 day 7: avoided senseless reparsing

--- solutions/2024/7/src/lib.rs
+++ solutions/2024/7/src/lib.rs
@@ -1,71 +1,62 @@
-fn parse(input: &str) -> impl Iterator<Item = (usize, impl Iterator<Item = usize> + Clone + '_)> {
+fn parse(input: &str) -> impl Iterator<Item = (usize, Vec<usize>)> + '_ {
     input.trim().lines().map(|l| {
         let (n, l) = l.split_once(':').unwrap();
         (
             n.trim().parse().unwrap(),
-            l.split_whitespace().map(|n| n.parse().unwrap()),
+            l.split_whitespace().map(|n| n.parse().unwrap()).collect(),
         )
     })
 }
 
 pub fn part1(input: &str) -> usize {
     let mut total = 0;
-    for (target, mut operands) in parse(input) {
-        if can_be_built(target, operands.next().unwrap(), operands) {
+    for (target, operands) in parse(input) {
+        if can_be_built(target, operands[0], &operands[1..]) {
             total += target
         }
     }
     total
 }
 
-fn can_be_built(
-    target: usize,
-    current: usize,
-    mut operands: impl Iterator<Item = usize> + Clone,
-) -> bool {
+pub fn part2(input: &str) -> usize {
+    let mut total = 0;
+    for (target, operands) in parse(input) {
+        if can_be_built_with_concat(target, operands[0], &operands[1..]) {
+            total += target
+        }
+    }
+    total
+}
+
+fn can_be_built(target: usize, current: usize, operands: &[usize]) -> bool {
     // We only have `+` and `*` so the current value can only grow
     // This would fail if a `0` was in the values, but the input does not contain one
     if current > target {
         return false;
     }
-    let Some(next) = operands.next() else {
+    let Some((next, operands)) = operands.split_first() else {
         return target == current;
     };
-    can_be_built(target, current + next, operands.clone())
-        || can_be_built(target, current * next, operands)
+    can_be_built(target, current + next, operands) || can_be_built(target, current * next, operands)
 }
 
-fn can_be_built_with_concat(
-    target: usize,
-    current: usize,
-    mut operands: impl Iterator<Item = usize> + Clone,
-) -> bool {
+fn can_be_built_with_concat(target: usize, current: usize, operands: &[usize]) -> bool {
     // We have `+`, `*` and `||` so the current value can only grow
     // This would fail if a `0` was in the values, but the input does not contain one
     if current > target {
         return false;
     }
-    let Some(next) = operands.next() else {
+    let Some((next, operands)) = operands.split_first() else {
         return target == current;
     };
-    can_be_built_with_concat(target, current + next, operands.clone())
-        || can_be_built_with_concat(target, current * next, operands.clone())
-        || can_be_built_with_concat(target, concat(current, next), operands)
+    can_be_built_with_concat(target, current + next, operands)
+        || can_be_built_with_concat(target, current * next, operands)
+        || can_be_built_with_concat(target, concat(current, *next), operands)
 }
 
 fn concat(a: usize, b: usize) -> usize {
     let b_digits = b.checked_ilog10().unwrap_or(0) + 1;
     a * 10usize.pow(b_digits) + b
-}
-
-pub fn part2(input: &str) -> usize {
-    let mut total = 0;
-    for (target, mut operands) in parse(input) {
-        if can_be_built_with_concat(target, operands.next().unwrap(), operands) {
-            total += target
-        }
-    }
-    total
 }
 
 #[cfg(test)]
